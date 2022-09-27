8db9b32a02c8434747a5af3e04335bcee7d79613
leomunizq <leonardo.mdq@hotmail.com>
2022-09-28 00:06:31 +0200

    home page e product page

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,3 +54,147 @@ window.onscroll = function () {
     nav.classList.remove('scroll-header')
   }
 }
+
+/*===== SPIN IMAGE =====*/
+
+$(function () {
+  var num = 36 // the total number of images
+
+  // Preload all the images into hidden div
+  for (var i = 01; i <= num; i++) {
+    var img = document.createElement('img')
+    img.src = '/assets/img/spin1/img' + i + '.jpg'
+    document.getElementById('preload-imgs').appendChild(img)
+  }
+
+  // Control swipes using jquery.touchSwipe.min.js
+  // http://labs.rampinteractive.co.uk/touchSwipe/
+  var swipeOptions = {
+    triggerOnTouchEnd: true,
+    swipeStatus: swipeStatus,
+    allowPageScroll: 'vertical',
+    threshold: 75
+  }
+
+  $(function () {
+    imgs = $('.home__shape') // the element that will be swipeable
+    imgs.swipe(swipeOptions)
+  })
+
+  function swipeStatus(event, phase, direction, distance) {
+    var duration = 0
+    if (direction == 'left') {
+      changeImg(distance)
+    } else if (direction == 'right') {
+      changeImgR(-distance)
+    }
+  }
+
+  function changeImg(imgNum) {
+    // divide by 8 (or any number) to spread
+    // it out so it doesn't load new img
+    // every single px of swipe distance
+    imgNum = Math.floor(imgNum / 8)
+
+    if (imgNum < 1) {
+      imgNum += num
+    }
+    if (imgNum > num) {
+      imgNum -= num
+    }
+
+    // change the image src
+    document.getElementById('myImg').src =
+      "/Users/leonardomuniz/Documents/projetos/Yeezy-Ecom/assets/img/spin1/img'+i+'.jpg"
+  }
+
+  function changeImgR(imgNum) {
+    // divide by 8 (or any number) to spread
+    // it out so it doesn't load new img
+    // every single px of swipe distance
+    imgNum = Math.floor(imgNum / 8)
+
+    var num2 = -Math.abs(num)
+    if (imgNum > num2) {
+      imgNum += num
+    }
+    if (imgNum <= num2) {
+      imgNum += num * 2
+    }
+
+    // change the image src
+    document.getElementById('myImg').src =
+      "/Users/leonardomuniz/Documents/projetos/Yeezy-Ecom/assets/img/spin1/img'+i+'.jpg"
+  }
+})
+
+/*===== SCROOLL REVEAL ANIMATION =====*/
+function teste() {
+  window.sr = ScrollReveal({ reset: false })
+
+  sr.reveal('.home__img', {
+    origin: 'right',
+    duration: 1200,
+    scale: 1,
+    distance: '200px',
+    useDelay: 'once'
+  })
+
+  sr.reveal('.home__shape', {
+    origin: 'left',
+    duration: 1200,
+    scale: 1,
+    distance: '200px',
+    useDelay: 'once'
+  })
+
+  sr.reveal('.home__data', {
+    origin: 'bottom',
+    duration: 1200,
+    scale: 1,
+    distance: '200px',
+    useDelay: 'once'
+  })
+
+  ScrollReveal({ reset: true })
+  ScrollReveal().reveal('.home__sneaker')
+  ScrollReveal().reveal('.sneaker', { delay: 200 })
+  ScrollReveal().reveal('.collection__card', {
+    delay: 200,
+    easing: 'cubic-bezier(0.5, 0, 0, 1)'
+  })
+}
+teste()
+
+/*===== PRODUCT PAGE =====*/
+
+window.onload = function () {
+  const colour_btn_els = document.querySelectorAll('.colours .colour')
+  const capacity_btn_els = document.querySelectorAll('.capacity .size')
+  const imagery_el = document.querySelector('.imagery')
+  const image_el = document.querySelector('.imagery .image')
+
+  for (let i = 0; i < capacity_btn_els.length; i++) {
+    let btn = capacity_btn_els[i]
+
+    btn.addEventListener('click', function () {
+      document
+        .querySelector('.capacity .size.selected')
+        .classList.remove('selected')
+      this.classList.add('selected')
+    })
+  }
+
+  for (let i = 0; i < colour_btn_els.length; i++) {
+    let btn = colour_btn_els[i]
+
+    btn.addEventListener('click', function () {
+      document
+        .querySelector('.colours .colour.selected')
+        .classList.remove('selected')
+      this.classList.add('selected')
+      image_el.src = 'assets/images/xr-' + this.dataset.name + '.png'
+      imagery_el.style.backgroundColor = this.dataset.colour
+    })
+  }
+}
